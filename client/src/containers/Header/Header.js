78eeae8db5e711948092bbf4574ae2800a54377c
harry <b05901059@ntu.edu.tw>
2022-06-29 18:02:04 +0800

    add pics to header

--- client/src/containers/Header/Header.js
+++ client/src/containers/Header/Header.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
-
+import Slider from 'react-slick'
 import './Header.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+// import pics here
+import img1 from "../../resource/IMG_1.JPG"
+import img2 from "../../resource/IMG_2.JPG"
 
 class Header extends Component {
     constructor(props) {
@@ -8,12 +14,24 @@ class Header extends Component {
     }
 
     render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+
         return(
             <div className="Header">
-                <div className="Pics">
-                    put pics here
-                </div>
-
+                <Slider {...settings} className="Pics">
+                        <div>
+                            <img src={img1} className="Pic"/>
+                        </div>
+                        <div>
+                            <img src={img2} className="Pic"/>
+                        </div>
+                </Slider>
                 <div className="LabName">
                     國立台灣大學 電子設計自動化實驗室
                 </div>
